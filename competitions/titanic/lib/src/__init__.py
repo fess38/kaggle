3ea889180cf71b3919842a75d47c519418855748
Yury Anapolskiy <fess38@gmail.com>
2023-10-29 17:43:07 +0200

    Remove old competitions implementation

--- competitions/titanic/lib/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import DvcConfig
-from .parquet_io import convert_table, read_parquet, write_parquet
-from .passenger import Label, Passenger
-from .transform import read_labels, read_passengers

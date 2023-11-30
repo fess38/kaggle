15b991ef41f620946fb876cb677c042173d8b6ee
Yury Anapolskiy <fess38@gmail.com>
2023-12-01 00:47:22 +0200

    Add linear regression

--- training/src/linear/config.py
+++ training/src/linear/config.py
@@ -6,6 +6,18 @@ from fess38.data_processing.operation.config import MapOpConfigBase
 from ..config import TrainOpConfigBase
 
 
+@operation_library("fess38.training.linear.linear_regression.LinearRegressionTrainOp")
+class LinearRegressionTrainOpConfig(TrainOpConfigBase):
+    type: Literal["linear_regression_trainer"] = "linear_regression_trainer"
+
+
+@operation_library(
+    "fess38.training.linear.linear_regression.LinearRegressionInferenceOp"
+)
+class LinearRegressionInferenceOpConfig(MapOpConfigBase):
+    type: Literal["linear_regression_inference"] = "linear_regression_inference"
+
+
 @operation_library(
     "fess38.training.linear.logistic_regression.LogisticRegressionTrainOp"
 )
---
+++ training/src/linear/linear_regression.py
@@ -0,0 +1,50 @@
+from typing import Iterable
+
+import joblib
+from fess38.data_processing.operation.mapper.base import MapOpBase
+from sklearn.linear_model import LinearRegression
+
+from ..base import TrainOpBase
+from ..types import PredictionRecord, SampleRecord
+from .config import LinearRegressionInferenceOpConfig, LinearRegressionTrainOpConfig
+
+
+class LinearRegressionTrainOp(TrainOpBase):
+    def __init__(self, config: LinearRegressionTrainOpConfig):
+        super().__init__(config, self._train_fn)
+
+    def _train_fn(self, records: Iterable[SampleRecord], role: str | None):
+        model = LinearRegression(**self.config.kwargs)
+
+        records = list(records)
+        model.fit(
+            X=[record.num_features for record in records],
+            y=[record.labels[0] for record in records],
+            sample_weight=(
+                [record.sample_weight for record in records]
+                if records[0].sample_weight is not None
+                else None
+            ),
+        )
+
+        joblib.dump(model, self.config.output_files["model.bin"])
+
+
+class LinearRegressionInferenceOp(MapOpBase):
+    def __init__(self, config: LinearRegressionInferenceOpConfig):
+        super().__init__(config, self._map_fn)
+
+    def _map_fn(
+        self,
+        records: Iterable[SampleRecord],
+        role: str | None,
+    ) -> Iterable[PredictionRecord]:
+        model: LinearRegression = joblib.load(self.config.input_files["model.bin"])
+        records = list(records)
+        for record in records:
+            predictions = model.predict([record.num_features]).tolist()
+            yield PredictionRecord(
+                id=record.id,
+                labels=record.labels,
+                predictions=predictions,
+            )

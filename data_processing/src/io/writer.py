a3dc371a375eee4bd2cb8ad8b5beb6c93e78d005
Yury Anapolskiy <fess38@gmail.com>
2024-01-18 22:24:40 +0200

    Move open file to the first read/write call

--- data_processing/src/io/writer.py
+++ data_processing/src/io/writer.py
@@ -1,9 +1,6 @@
 import abc
 from pathlib import Path
-from typing import Any
-
-import fsspec
-from fess38.util.filesystem import fs_for_path
+from typing import IO, Any
 
 from .common import FileDatasetIOMixin
 from .dataset_reference import FileOutputDatasetReference, OutputDatasetReference
@@ -22,35 +19,34 @@ class DatasetWriterBase(abc.ABC):
 class FileDatasetWriter(DatasetWriterBase, FileDatasetIOMixin):
     def __init__(self, dataset_reference: FileOutputDatasetReference):
         self._dataset_reference = dataset_reference
-        self._validate_record_class()
-        fs = fs_for_path(self.data_path)
-        self._create_dataset_directory(fs)
-        self._file = fs.open(
-            self.data_path,
-            mode=self.record_formatter.write_mode,
-        )
+        self._data_file: IO = None
         self._records = []
+        self._validate_record_class()
+        self._validate_allow_overwrite()
 
     def write(self, record: Any):
+        if self._data_file is None:
+            self._fs.makedirs(Path(self.data_path).parent, exist_ok=True)
+            self._data_file = self._fs.open(
+                self.data_path, mode=self.record_formatter.write_mode
+            )
+
         self._records.append(record)
 
     def close(self):
-        self.record_formatter.write(self._file, self._records)
-        self._records.clear()
-        self._file.close()
+        if self._data_file is not None and not self._data_file.closed:
+            self.record_formatter.write(self._data_file, self._records)
+            self._records.clear()
+            self._data_file.close()
 
-    def _create_dataset_directory(self, fs: fsspec.AbstractFileSystem):
-        if not self.dataset_reference.allow_overwrite and fs.exists(self.data_path):
+    def _validate_allow_overwrite(self):
+        if not self._dataset_reference.allow_overwrite and self._fs.exists(
+            self.data_path
+        ):
             raise ValueError(
-                f"Output dataset {self.data_path} already exists and overwriting is"
+                f"Output dataset '{self.data_path}' already exists and overwriting is"
                 " disabled."
             )
-
-        fs.makedirs(Path(self.data_path).parent, exist_ok=True)
-
-    @property
-    def dataset_reference(self) -> FileOutputDatasetReference:
-        return self._dataset_reference
 
 
 def create_dataset_writer(

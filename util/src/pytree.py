6ba34f8a55e22591349593dd8fa67a723195ee53
Yury Anapolskiy <fess38@gmail.com>
2024-01-28 19:49:38 +0200

    Add pytree modification functions

--- util/src/pytree.py
+++ util/src/pytree.py
@@ -1,26 +1,53 @@
 from typing import Any
 
-from .typing import PyTreePath
+from .typing import PyTree, PyTreePath
 
 
 def normalize_path(path: PyTreePath) -> PyTreePath:
-    return (path,) if isinstance(path, str) else path
+    return tuple(path.split(".")) if isinstance(path, str) else path
 
 
-def format_path(path: PyTreePath) -> str:
-    path = normalize_path(path)
-    return "=>".join([f"'{field}'" for field in path])
+def get_field_by_path_safe(tree: PyTree, path: PyTreePath) -> tuple[bool, Any]:
+    for path_part in normalize_path(path):
+        if not isinstance(tree, dict) or path_part not in tree:
+            return False, None
+        tree = tree[path_part]
+
+    return True, tree
 
 
-def get_field_by_path(tree: Any, path: PyTreePath) -> Any:
-    path = normalize_path(path)
+def get_field_by_path(tree: PyTree, path: PyTreePath) -> Any:
+    def _format_path(path: PyTreePath) -> str:
+        path = normalize_path(path)
+        return "=>".join([f"'{field}'" for field in path])
 
-    for i in range(len(path)):
-        field = path[i]
-        if field not in tree:
-            raise ValueError(
-                f"Path {format_path(path[:i + 1])} not found in the tree {tree}."
-            )
-        tree = tree[field]
+    path_exists, value = get_field_by_path_safe(tree, path)
+    if not path_exists:
+        raise ValueError(f"Path {_format_path(path)} not found in the tree {tree}.")
 
-    return tree
+    return value
+
+
+def set_field_by_path(tree: PyTree, path: PyTreePath, value: Any):
+    path_parts = normalize_path(path)
+
+    subtree = tree
+    for path_part in path_parts[:-1]:
+        if path_part not in subtree:
+            subtree[path_part] = {}
+        subtree = subtree[path_part]
+
+    subtree[path_parts[-1]] = value
+
+
+def delete_field_by_path(tree: PyTree, path: PyTreePath):
+    path_parts = normalize_path(path)
+
+    subtree = tree
+    for path_part in path_parts[:-1]:
+        if not isinstance(subtree, dict) or path_part not in subtree:
+            return
+        tree = tree[path_part]
+
+    if isinstance(tree, dict) and path_parts[-1] in tree:
+        del tree[path_parts[-1]]
--- util/tests/test_pytree.py
+++ util/tests/test_pytree.py
@@ -1,22 +1,88 @@
+from typing import Any
+
 import pytest
-from fess38.util.pytree import format_path, get_field_by_path, normalize_path
+from fess38.util.pytree import (
+    delete_field_by_path,
+    get_field_by_path,
+    get_field_by_path_safe,
+    normalize_path,
+    set_field_by_path,
+)
+from fess38.util.typing import PyTree, PyTreePath
 
 
-def test_normalize_path():
-    assert normalize_path("foo") == ("foo",)
-    assert normalize_path(("foo", "bar")) == ("foo", "bar")
+@pytest.mark.parametrize(
+    ("path", "expected"),
+    [
+        ("foo", ("foo",)),
+        (("foo", "bar"), ("foo", "bar")),
+        ("foo.bar", ("foo", "bar")),
+    ],
+)
+def test_normalize_path(path: PyTreePath, expected: tuple[str]):
+    assert normalize_path(path) == expected
 
 
-def test_format_path():
-    assert format_path("foo") == "'foo'"
-    assert format_path(("foo", "bar")) == "'foo'=>'bar'"
+@pytest.mark.parametrize(
+    ("tree", "path", "expected"),
+    [
+        ({"a": {"b": 1}}, "a.b", (True, 1)),
+        ({"a": {"b": {}}}, "a.b", (True, {})),
+        ({"a": {"b": 1}}, "a.b.c", (False, None)),
+        ({"a": {"b": 1}}, "c", (False, None)),
+    ],
+)
+def test_get_field_by_path_safe(
+    tree: PyTree, path: PyTreePath, expected: tuple[bool, Any]
+):
+    assert get_field_by_path_safe(tree, path) == expected
 
 
-def test_get_field_by_path():
-    assert get_field_by_path({"foo": {"bar": 123, "a": "b"}}, ("foo", "bar")) == 123
-    assert get_field_by_path({"foo": {"a": "b"}}, "foo") == {"a": "b"}
+@pytest.mark.parametrize(
+    ("tree", "path", "is_raise", "expected"),
+    [
+        ({"a": {"b": 1}}, "a.b", False, 1),
+        ({"a": {"b": {}}}, "a.b", False, {}),
+        ({"a": {"b": 1}}, "a.b.c", True, None),
+        ({"a": {"b": 1}}, "c", True, None),
+    ],
+)
+def test_get_field_by_path(
+    tree: PyTree, path: PyTreePath, is_raise: bool, expected: Any
+):
+    if is_raise:
+        with pytest.raises(ValueError):
+            get_field_by_path(tree, path)
+        return
+
+    assert get_field_by_path(tree, path) == expected
 
 
-def test_get_field_by_path_raises():
-    with pytest.raises(ValueError):
-        get_field_by_path({"foo": {"a": "b"}}, "b")
+@pytest.mark.parametrize(
+    ("tree", "path", "value"),
+    [
+        ({"a": {"b": 1}}, "a.c", "q"),
+        ({"a": {"b": {}}}, "a.b.c", 1),
+        ({"a": {"b": 1}}, "a.c.b", "z"),
+        ({"a": {"b": 1}}, "c", False),
+    ],
+)
+def test_set_field_by_path(tree: PyTree, path: PyTreePath, value: Any):
+    set_field_by_path(tree, path, value)
+    assert value == get_field_by_path(tree, path)
+
+
+@pytest.mark.parametrize(
+    ("tree", "path"),
+    [
+        ({"a": {"b": 1}}, "a"),
+        ({"a": {"b": 1}}, "a.b"),
+        ({"a": {"b": 1}}, "a.c"),
+        ({"a": {"b": {}}}, "a.b.c"),
+        ({"a": {"b": 1}}, "a.c.b"),
+        ({"a": {"b": 1}}, "c"),
+    ],
+)
+def test_delete_field_by_path(tree: PyTree, path: PyTreePath):
+    delete_field_by_path(tree, path)
+    assert not get_field_by_path_safe(tree, path)[0]
